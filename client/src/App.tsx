0cac74f6992e7a8ea986391b795a8f224370f238
Khaled Badran <gym4programming@gmail.com>
2023-01-23 20:26:44 -0700

    feat: adding new pages and components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,35 @@
+import { FC } from "react";
+import AnnouncementBar from "./components/AnnouncementBar";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Product from "./pages/Product";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Cart from "./pages/Cart";
 
-function App() {
-    return <Home />;
-}
+const App: FC = () => {
+    // return <Home />;
+    return (
+        <div>
+            <AnnouncementBar
+                announcement={
+                    "NEW YEAR DEAL: 10% OFF EVERYTHING + FREE SHIPPING"
+                }
+            />
+            <Navbar />
+
+            {/* <Home/> */}
+            {/* <Cart />; */}
+            {/* <Category />; */}
+            {/* <Product /> */}
+            <SignUp />
+            <SignIn />
+
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
