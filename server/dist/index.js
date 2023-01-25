524fc456f6f6b67a37bb8625f38197bd3bde137e
Khaled Badran <gym4programming@gmail.com>
2023-01-24 22:15:22 -0700

    build: autogenerated compiled ts to js files

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,12 +4,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors")); // cors middleware to avoid having any cors bugs
+const Users_1 = require("./routes/Users");
+const Auth_1 = require("./routes/authentication/Auth");
+const Products_1 = require("./routes/Products");
+const Carts_1 = require("./routes/Carts");
+const Orders_1 = require("./routes/Orders");
+const Stripe_1 = require("./routes/payment-gateway/Stripe");
 const app = (0, express_1.default)();
-const port = 5000;
-app.get('/', (req, res) => {
-    res.send("Welcome To e-shop-by-khaled");
-});
-app.listen(port, () => {
-    console.log("Currently listening on port " + port);
+dotenv_1.default.config(); // initiate environmental keys and constants
+const connectToMongoDb = async () => {
+    try {
+        mongoose_1.default.set("strictQuery", true);
+        const db = await mongoose_1.default.connect(process.env.MONGODB_CONNECTION_STRING);
+        console.log("successfully connected to db.");
+    }
+    catch (error) {
+        console.log("couldn't connected to db - ", error);
+    }
+};
+connectToMongoDb();
+app.use((0, cors_1.default)()); // cors middleware to avoid having any cors bugs
+app.use(express_1.default.json()); // to accept JSON data requests for the API
+app.use("/api/users", Users_1.router);
+app.use("/api/auth", Auth_1.router);
+app.use("/api/products", Products_1.router);
+app.use("/api/carts", Carts_1.router);
+app.use("/api/orders", Orders_1.router);
+app.use("/api/checkout", Stripe_1.router);
+app.listen(process.env.PORT, () => {
+    console.log("successfully listening on port.");
 });
 //# sourceMappingURL=index.js.map
