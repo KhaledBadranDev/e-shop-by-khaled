251dbf6ea7384b992aa4171ff20ebadede802b3c
Khaled Badran <gym4programming@gmail.com>
2023-02-15 06:41:29 -0700

    feat: add routes and redux store, user and cart

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,10 @@
 import { FC } from "react";
+import {
+    BrowserRouter as RouterDom,
+    Routes,
+    Route,
+    Navigate ,
+} from "react-router-dom";
 import AnnouncementBar from "./components/AnnouncementBar";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -8,9 +14,21 @@ import Product from "./pages/Product";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import { IReduxUserStateType } from "./redux/user";
+import { useSelector } from "react-redux";
+import { IReduxRootStateType } from "./redux/store";
+
+// *###########################################################################
+// *Note: the front-end pages routes are different from the api/back-end routes
+// *###########################################################################
 
 const App: FC = () => {
-    // return <Home />;
+    const currentUserRedux: IReduxUserStateType = useSelector<
+        IReduxRootStateType,
+        IReduxUserStateType
+    >((state: any) => state.user.username);
+
     return (
         <div>
             <AnnouncementBar
@@ -18,14 +36,31 @@ const App: FC = () => {
                     "NEW YEAR DEAL: 10% OFF EVERYTHING + FREE SHIPPING"
                 }
             />
-            <Navbar />
-
-            {/* <Home/> */}
-            {/* <Cart />; */}
-            {/* <Category />; */}
-            {/* <Product /> */}
-            <SignUp />
-            <SignIn />
+            <RouterDom>
+                <Navbar />
+                <Routes>
+                    {/* /: this means parameter */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route
+                        path="/categories/:categoryName"
+                        element={<Category />}
+                    />
+                    <Route
+                        path="/signin"
+                        element={
+                            currentUserRedux ? <Navigate  to="/" /> : <SignIn />
+                        }
+                    />
+                    <Route
+                        path="/signup"
+                        element={currentUserRedux ? <Navigate  to="/" /> : <SignUp />}
+                    />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/products/:productId" element={<Product />} />
+                </Routes>
+            </RouterDom>
 
             <Footer />
         </div>
