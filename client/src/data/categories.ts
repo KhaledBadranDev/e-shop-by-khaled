9b10633aeacea330b2d4da94a1074c6a02b1cac7
Khaled Badran <gym4programming@gmail.com>
2023-02-15 07:02:09 -0700

    feat: update categories data

--- client/src/data/categories.ts
+++ client/src/data/categories.ts
@@ -2,6 +2,7 @@ interface Category {
     id: number;
     imgReference: string;
     title: string;
+    categoryName: string;
 }
 
 const categories: Category[] = [
@@ -10,18 +11,21 @@ const categories: Category[] = [
         imgReference:
             "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         title: "SHIRT STYLE!",
+        categoryName: "men",
     },
     {
         id: 2,
         imgReference:
             "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         title: "LOUNGEWEAR LOVE",
+        categoryName: "women",
     },
     {
         id: 3,
         imgReference:
             "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
         title: "LIGHT JACKETS",
+        categoryName: "skirt",
     },
 ];
 
