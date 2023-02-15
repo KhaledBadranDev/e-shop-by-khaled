8a3901900f8e2b3b765135136b71e15840557512
Khaled Badran <gym4programming@gmail.com>
2023-02-15 06:31:47 -0700

    feat: add admin sign in page

---
+++ admin/src/pages/SignIn.tsx
@@ -0,0 +1,146 @@
+import { FC, useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import reduxSignInAsyncAPICallHelper from "../api/reduxAPIRequests";
+import { IReduxRootStateType } from "../redux/store";
+import { IReduxAdminStateType } from "../redux/admin";
+
+const SignIn: FC = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const adminReduxStateDispatch = useDispatch(); // "useDispatch"  to change the value of the global states
+    // to access the admin global state using redux
+    const adminRedux: IReduxAdminStateType = useSelector<
+        IReduxRootStateType,
+        IReduxAdminStateType
+    >((state: any) => state.admin);
+
+    // using arrow functions or binding in tSX is a bad practice as it hurts the performance.
+    // because the function is recreated on each render.
+    // to solve this issue, use the callback with the useCallback() hook,
+    // and assign the dependencies.
+    const handleSetEmail = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            setEmail(event.target.value);
+        },
+        [email]
+    );
+
+    const handleSetPassword = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            setPassword(event.target.value);
+        },
+        [password]
+    );
+
+    const handelSignIn = useCallback(
+        (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+            event.preventDefault();
+            reduxSignInAsyncAPICallHelper(adminReduxStateDispatch, {
+                email,
+                password,
+            });
+        },
+        [email, password]
+    );
+    return (
+        <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input
+                        required
+                        placeholder="email"
+                        onChange={handleSetEmail}
+                    />
+                    <Input
+                        required
+                        placeholder="password"
+                        type="password"
+                        onChange={handleSetPassword}
+                    />
+                    <Button
+                        onClick={(event) => handelSignIn(event)}
+                        disabled={adminRedux.isAsyncSigningIn}
+                    >
+                        Sign In
+                    </Button>
+
+                    {
+                        //  conditional rendering if there is any error
+                        adminRedux.error.length > 0 && (
+                            <Error>{adminRedux.error}</Error>
+                        )
+                    }
+                    <Link>FORGOT YOUR PASSWORD?</Link>
+                    <Link>CREATE A NEW ACCOUNT</Link>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default SignIn;
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+            rgba(255, 255, 255, 0.5),
+            rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+            center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Wrapper = styled.div`
+    width: 25%;
+    padding: 20px;
+    background-color: white;
+`;
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0;
+    padding: 10px;
+`;
+
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 10px;
+    &:disabled {
+        color: transparent;
+        cursor: not-allowed;
+    }
+`;
+
+const Error = styled.span`
+    color: red;
+`;
+
+const Link = styled.a`
+    margin: 5px 0px;
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
+`;
