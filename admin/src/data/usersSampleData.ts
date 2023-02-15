04269e48e2cebe3c36b583fa152e037f47df3d14
Khaled Badran <gym4programming@gmail.com>
2023-02-15 06:29:10 -0700

    test: add dummy/sample data for users

---
+++ admin/src/data/usersSampleData.ts
@@ -0,0 +1,93 @@
+interface IUserType {
+    id: number;
+    username: string;
+    profilePicRef: string;
+    email: string;
+    lastTransaction: string;
+}
+
+const data: IUserType[] = [
+    {
+        id: 1,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 2,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 3,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 4,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 5,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 6,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 7,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 8,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 9,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+    {
+        id: 10,
+        username: "Jon Snow",
+        profilePicRef:
+            "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "jon@gmail.com",
+        lastTransaction: "$120.00",
+    },
+];
+
+export default data;
+export type { IUserType };
