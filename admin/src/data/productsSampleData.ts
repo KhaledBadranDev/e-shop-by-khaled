4e99008d044bfc950efbd196fe629084bc45040f
Khaled Badran <gym4programming@gmail.com>
2023-02-15 06:29:21 -0700

    test: add dummy/sample data for products

---
+++ admin/src/data/productsSampleData.ts
@@ -0,0 +1,93 @@
+interface IProductType {
+    _id: number;
+    title: string;
+    imgReference: string;
+    price: number;
+    isInStock: boolean;
+}
+
+const data: IProductType[] = [
+    {
+        _id: 1,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 2,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 3,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 4,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 5,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 6,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 7,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 8,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 9,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+    {
+        _id: 10,
+        title: "Apple Airpods",
+        imgReference:
+            "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        isInStock: true,
+        price: 120.0,
+    },
+];
+
+export default data;
+export type { IProductType };
